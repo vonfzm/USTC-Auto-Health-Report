ec0506c416365d92655382fdf35802b2c9435ff7
Zimou Feng <fzm1011@gmail.com>
2022-04-28 21:27:29 +0800

    两码更新

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from ustc_auto_report import USTCAutoHealthReport
 import random,time
 # 随机时间延迟，单位为秒
-sleeptime=random.randint(0, 300)
-time.sleep(sleeptime)
+time.sleep(random.randint(0, 300))
 bot = USTCAutoHealthReport()
 # 登录
 bot.login('你的学号', '你的密码')
 # 打卡
 bot.daily_clock_in('post.json')
+# 上传两码
+bot.upload_code()
+time.sleep(random.randint(60, 62))
 # 进出校申请
 bot.stayinout_apply('apply.json')
