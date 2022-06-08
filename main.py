8ced7f2ea9f5ef1e7d5192c1c64489a2f9ee0eb6
Zimou Feng <fzm1011@gmail.com>
2022-06-08 18:10:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ bot.login('你的学号', '你的密码')
 # 打卡
 bot.daily_clock_in('post.json')
 # 上传两码
-# bot.upload_code()
-# time.sleep(random.randint(60, 62))
+bot.upload_code()
+time.sleep(random.randint(60, 62))
 # 进出校申请
 bot.stayinout_apply('apply.json')
